4e1b53864b1dd33a15bd3aa2ca20ad3010d3e03a
kulturman <arnaudbakyono@gmail.com>
2024-09-06 17:32:33 +0000

    Use tap in map example

--- map.ts
+++ map.ts
@@ -1,8 +1,10 @@
-import {from, map} from "rxjs";
+import {from, map, tap} from "rxjs";
 
 from([1, 2, 3, 4, 5, 6, 7, 8]).pipe(
+    tap(value => console.log(value)),
+
     map(value => ({
         value,
         isEven: value % 2 === 0
-    }))
+    })),
 ).subscribe(value => console.log(value));
